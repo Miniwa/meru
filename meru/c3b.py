6d06f6ed3f4184734824bdeb4c90b05143292dc6
miniwa <blarch3030@gmail.com>
2018-07-02 11:32:59 +0200

    Added support for material command

--- meru/c3b.py
+++ meru/c3b.py
@@ -16,7 +16,7 @@ class C3bType(Enum):
     ANIMATION = 4
     ANIMATION_CHANNEL = 5
     MODEL = 10
-    MATERIAL = 16
+    MATERIALS = 16
     EFFECT = 18
     CAMERA = 32
     LIGHT = 33
@@ -41,9 +41,67 @@ class C3bHeader:
         self.references = references
 
 
+class C3bMesh:
+    def __init__(self):
+        self.vertex_arrays = []
+
+
+class C3bVertexArray:
+    def __init__(self):
+        self.attributes = []
+        self.values = []
+        self.shapes = []
+
+    def values_per_vertex(self):
+        value_count = 0
+        for attrib in self.attributes:
+            value_count += attrib.nr_values
+        return value_count
+
+    def vertex_count(self):
+        return len(self.values) / self.values_per_vertex()
+
+
+class C3bVertexAttribute:
+    def __init__(self, nr_values, _type, name):
+        self.nr_values = nr_values
+        self.type = _type
+        self.name = name
+
+
+class C3bShape:
+    def __init__(self, _id):
+        self.id = _id
+        self.indices = []
+        self.aabb = []
+
+
+class C3bMaterial:
+    def __init__(self, _id):
+        self.id = _id
+        self.textures = []
+
+
+class C3bTexture:
+    def __init__(self, _id, filename, _type, wrap_s, wrap_t):
+        self.id = _id
+        self.filename = filename
+        self.type = _type
+        self.wrap_s = wrap_s
+        self.wrap_t = wrap_t
+
+
 class C3bParser:
     def __init__(self, _bytes):
         self._reader = BinaryReader(_bytes)
+        self.endianness = Endian.LITTLE
+
+    @classmethod
+    def from_file(self, filename):
+        with open(filename, "rb") as _file:
+            buffer = _file.read()
+            parser = C3bParser(buffer)
+        return parser
 
     def verify_signature(self):
         self._reader.seek(0)
@@ -53,11 +111,103 @@ class C3bParser:
         self._reader.seek(4)
         major_version = self._reader.read_int8()
         minor_version = self._reader.read_int8()
+
         references = []
-        reference_count = self._reader.read_uint32(Endian.LITTLE)
+        reference_count = self._read_uint()
         for i in range(reference_count):
-            id = self._reader.read_prefixed_string_uint32(Endian.LITTLE)
-            _type = self._reader.read_uint32(Endian.LITTLE)
-            offset = self._reader.read_uint32(Endian.LITTLE)
-            references.append(C3bReference(id, C3bType(_type), offset))
+            _id = self._read_string()
+            _type = self._read_uint()
+            offset = self._read_uint()
+            references.append(C3bReference(_id, C3bType(_type), offset))
         return C3bHeader(major_version, minor_version, references)
+
+    def read_mesh(self, index):
+        self.seek_type(C3bType.MESH, index)
+        mesh = C3bMesh()
+
+        # Read vertex arrays
+        vertex_arr_count = self._read_uint()
+        for vertex_arr_index in range(vertex_arr_count):
+            vertex_array = C3bVertexArray()
+
+            # Read attributes
+            attrib_count = self._read_uint()
+            for attrib_index in range(attrib_count):
+                nr_values = self._read_uint()
+                _type = self._read_string()
+                name = self._read_string()
+                vertex_array.attributes.append(
+                    C3bVertexAttribute(nr_values, _type, name))
+
+            # Read vertices
+            value_count = self._read_uint()
+            for value_index in range(value_count):
+                vertex_value = self._reader.read_float32(self.endianness)
+                vertex_array.values.append(vertex_value)
+
+            # Read shapes
+            shape_count = self._read_uint()
+            for shape_index in range(shape_count):
+                _id = self._read_string()
+                shape = C3bShape(_id)
+
+                # Read indices
+                index_count = self._read_uint()
+                for i in range(index_count):
+                    _index = self._reader.read_uint16(self.endianness)
+                    shape.indices.append(_index)
+
+                # Read axis aligned bounding box
+                for aabb in range(6):
+                    aabb_coord = self._reader.read_float32(Endian.LITTLE)
+                    shape.aabb.append(aabb_coord)
+                vertex_array.shapes.append(shape)
+            mesh.vertex_arrays.append(vertex_array)
+        return mesh
+
+    def read_materials(self, index):
+        self.seek_type(C3bType.MATERIALS, index)
+        materials = []
+
+        material_count = self._read_uint()
+        for material_index in range(material_count):
+            _id = self._read_string()
+            material = C3bMaterial(_id)
+
+            # Skip diffuse(3 float), ambient(3 float), emissive(3 float),
+            # opacity(1 float), specular(3 float), shininess(1 float)
+            self._reader.strict_read(14 * 4)
+
+            texture_count = self._read_uint()
+            for texture_index in range(texture_count):
+                texture_id = self._read_string()
+                texture_filename = self._read_string()
+
+                # Skip UV data (4 float), handled by vertex attribute?
+                self._reader.read(4 * 4)
+
+                texture_type = self._read_string()
+                texture_wrap_s = self._read_string()
+                texture_wrap_t = self._read_string()
+                texture = C3bTexture(texture_id, texture_filename,
+                    texture_type, texture_wrap_s, texture_wrap_t)
+                material.textures.append(texture)
+            materials.append(material)
+        return materials
+
+    def seek_type(self, _type, index):
+        refs = self.read_header().references
+        filtered = list(filter(lambda ref: ref.type == _type, refs))
+        if index >= len(filtered):
+            raise IndexError("{0} index out of bounds, max {1}."
+                .format(_type.name, len(filtered) - 1))
+        self._reader.seek(filtered[index].offset)
+
+    def _read_uint(self):
+        return self._reader.read_uint32(self.endianness)
+
+    def _read_ushort(self):
+        return self._reader.read_uint16(self.endianness)
+
+    def _read_string(self):
+        return self._reader.read_prefixed_string_uint32(self.endianness)
--- meru/meru.py
+++ meru/meru.py
@@ -15,7 +15,7 @@ def main():
 
 @main.command()
 @click.argument("filenames", type=click.Path(exists=True), nargs=-1)
-def scan(filenames):
+def header(filenames):
     for filename in filenames:
         with open(filename, "rb") as _file:
             buffer = _file.read()
@@ -32,11 +32,49 @@ def scan(filenames):
         print("RefCount: {0}".format(len(header.references)))
 
         for ref in header.references:
-            print(" ID: {0}".format(ref.id))
-            print(" Type: {0}".format(ref.type.name))
-            print(" Offset: {0}".format(ref.offset))
+            print("ID: {0}".format(ref.id))
+            print("Type: {0}".format(ref.type.name))
+            print("Offset: {0}".format(ref.offset))
             print()
         print()
+
+
+@main.command()
+@click.argument("filenames", type=click.Path(exists=True), nargs=-1)
+def mesh(filenames):
+    for filename in filenames:
+        parser = C3bParser.from_file(filename)
+        if not parser.verify_signature():
+            print("ERROR: {0} is not a c3b file.".format(filename))
+            continue
+
+        mesh = parser.read_mesh(0)
+        print("File: {0}".format(filename))
+        print("VertexArrayCount: {0}".format(len(mesh.vertex_arrays)))
+        for vertex_array in mesh.vertex_arrays:
+            print()
+            print("VertexCount: {0}".format(vertex_array.vertex_count()))
+            print("AttributeCount: {0}".format(len(vertex_array.attributes)))
+            for attribute in vertex_array.attributes:
+                print()
+                print("Name: {0}".format(attribute.name))
+                print("Type: {0}".format(attribute.type))
+                print("NrValues: {0}".format(attribute.nr_values))
+            print()
+            print("ShapeCount: {0}".format(len(vertex_array.shapes)))
+            for shape in vertex_array.shapes:
+                print()
+                print("ID: {0}".format(shape.id))
+                print("IndexCount: {0}".format(len(shape.indices)))
+
+
+@main.command()
+@click.argument("filenames", type=click.Path(exists=True), nargs=-1)
+def materials(filenames):
+    for filename in filenames:
+        parser = C3bParser.from_file(filename)
+        materials = parser.read_materials(0)
+        print(materials)
 
 
 if __name__ == '__main__':
